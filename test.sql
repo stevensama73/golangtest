49a34f383af2fd169dd8e836eaf74d4ede2c11c3
Steven <62057156+stevensama73@users.noreply.github.com>
2023-12-25 20:55:17 +0700

    Update test.sql

--- test.sql
+++ test.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `dbprovider`.`providers` (
   `provider` VARCHAR(32) NOT NULL, 
-  `no_hp` VARCHAR(5000) NOT NULL, 
+  `no_hp` VARCHAR(255) NOT NULL, 
   PRIMARY KEY (`provider`, `no_hp`)
 ) ENGINE = InnoDB;
