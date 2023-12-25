0e22b498713f3a3a24257c70fbe107004079fde0
stevensama73 <stevensama73@gmail.com>
2023-12-25 15:38:12 +0700

    finish

--- test.sql
+++ test.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `dbprovider`.`providers` (
   `provider` VARCHAR(32) NOT NULL, 
-  `no_hp` VARCHAR(32) NOT NULL, 
+  `no_hp` TEXT NOT NULL, 
   PRIMARY KEY (`provider`, `no_hp`)
 ) ENGINE = InnoDB;
